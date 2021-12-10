ad0f4c0074c1dfad28f858cf2a5cd025daa2c6da
hairunisa29 <hairunisa2909@outlook.com>
2021-12-10 10:03:38 +0700

    Mengupload file modal with multiple cards

---
+++ src/component/ModalCards.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './ProfileCard.css'
+import CardData from './CardData';
+
+
+function ModalCards(props){
+   
+    // console.log(props.fullname)
+    // console.log(props.dateOfBirth)
+    console.log(props.data)
+    return (
+            <div id="previewCard">
+                
+                {props.data.map(data => (
+                    <div className="card-data" key={data.id}>
+                        <CardData onClose={data.closeHandler} key={data.length} fullname={data.fullname} dateOfBirth={data.dateOfBirth} company={data.company} phonenum={data.phonenum} role={data.role} profilepic={data.profilepic} file={data.file}/>
+                    </div>
+                    ))
+                                
+                }
+                
+                  
+                
+              </div>
+            
+            
+
+        
+    )
+}
+
+export default ModalCards;
